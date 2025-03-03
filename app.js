7e04a8a9994b776a57941d210de7db22cbb396ba
shrutinimai <nimaishruti@gmail.com>
2025-03-03 13:21:04 +0530

    adding or creating  middleware

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 
-const PORT = 6000;
+const PORT = 3200;
+
+const addUserMiddleware=((req,res,next)=>{
+    req.user="Guest";
+    next();
+});
+app.use("/welcome", addUserMiddleware);
+
+
+app.get("/welcome",(req,res)=>{
+    res.send(`<h1>welcome, ${req.user}!</h1>`);
+})
+
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
